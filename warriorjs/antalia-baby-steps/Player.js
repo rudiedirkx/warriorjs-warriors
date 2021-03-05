ee4715a175de915cfeff1fcc2854ae0059c4f3cc
Rudie Dirkx <github@hotblocks.nl>
2021-03-06 00:50:43 +0100

    finished level 4

--- warriorjs/antalia-baby-steps/Player.js
+++ warriorjs/antalia-baby-steps/Player.js
@@ -1,16 +1,54 @@
 class Player {
 	playTurn(warrior) {
+		this.warrior = warrior;
+
+		const healthLost = this.healthLost(warrior);
 		const feeling = warrior.feel();
-		if (feeling.isEmpty()) {
+		this.killedEnemy = this.prevMove == 'attack' && feeling.isEmpty();
+this.killedEnemy && warrior.think('killed enemy');
+		this.killedEnemy && (this.facingEnemy = null);
+		const isArcher = this.facingEnemy == 'archer' || this.isArcher(healthLost, feeling);
+warrior.think(isArcher ? 'is archer' : 'is not archer');
+this.facingEnemy && warrior.think('facing ' + this.facingEnemy);
+
+		if (healthLost > 0 && !isArcher) {
+			this.prevMove = 'backward';
+			warrior.walk('backward');
+		}
+		else if (feeling.isEmpty()) {
 			if (warrior.health() < warrior.maxHealth()) {
+				this.prevMove = 'rest';
 				warrior.rest();
 			}
 			else {
+				this.prevMove = 'forward';
 				warrior.walk();
 			}
 		}
 		else {
+			this.prevMove = 'attack';
+			this.facingEnemy = isArcher ? 'archer' : 'sludge';
 			warrior.attack();
 		}
+
+		this.prevEmpty = feeling.isEmpty();
+		this.prevHealth = warrior.health();
+	}
+
+	log(msg) {
+		this.warrior.think(msg.toUpperCase ? msg.toUpperCase() : msg);
+	}
+
+	isArcher(healthLost, feeling) {
+		return healthLost > 0 && (feeling.isEmpty() || this.prevEmpty);
+	}
+
+	healthLost(warrior) {
+		if (this.prevHealth == null) {
+			return 0;
+		}
+
+		const healthLost = this.prevHealth - warrior.health();
+		return Math.max(0, healthLost);
 	}
 }
